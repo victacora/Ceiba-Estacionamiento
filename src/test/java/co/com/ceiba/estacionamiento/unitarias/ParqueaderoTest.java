a544413ceed405792d2d23cbec97e32f41fe5054
Víctor Alfonso Puyo Epia - Ceiba Software <victor.puyo@ceiba.com.co>
2018-06-26 08:38:13 -0500

    Creacion de pruebas unitarias inciales

---
+++ src/test/java/co/com/ceiba/estacionamiento/unitarias/ParqueaderoTest.java
@@ -0,0 +1,79 @@
+package co.com.ceiba.estacionamiento.unitarias;
+
+import org.junit.Test;
+
+public class ParqueaderoTest {
+
+	@Test
+	public void InsertarCarro() 
+	{
+		
+	}
+
+	@Test
+	public void InsertarMoto() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCuposCarroMax20() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCuposMotoMax10() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarAccesoVehiculoPlacasTerminadasEnA() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroHoraCarro() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroDiaCarro() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroHoraMoto() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroDiaMoto() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroMotoCilindrajeMayor500CC() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroCarroParaVariosDiasYHoras() 
+	{
+		
+	}
+	
+	@Test
+	public void ValidarCobroMotoParaVariosDiasYHoras() 
+	{
+		
+	}
+	
+}
